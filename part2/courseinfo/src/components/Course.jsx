ab62b4042cee05bbdac6b00f1d4b96f935be544f
hamishmcgregor3 <hmcgregor@wustl.edu>
2025-01-20 20:00:54 +1100

    Commit for part2 courseinfo project (ex 2.1-2.5)

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,45 @@
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Content = ({ parts }) => {
+    return (
+        <ul>
+            {parts.map(part =>
+                <li key={part.id}>
+                    <Part name={part.name} exercises={part.exercises} />
+                </li>
+            )}
+        </ul>
+    )
+}
+
+const Part = ({ name, exercises }) => {
+    return (
+        <p>{name} {exercises}</p>
+    )
+}
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => {
+        return sum + part.exercises
+    }, 0)
+
+    return (
+        <div>
+            <p>
+                <strong>Total of {total} exercises</strong>
+            </p>
+        </div>
+    )
+}
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
